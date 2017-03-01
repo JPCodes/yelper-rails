260f7ca0fd2281620da62938159e3d0432e0d7e0
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-03-01 12:35:07 -0800

    Remove comments

--- app/services/yelp_response.rb
+++ app/services/yelp_response.rb
@@ -9,16 +9,13 @@ class YelpResponse
     if @body.split.length == 2
       api_response = get_restaurants(@body.downcase.split[0], @body.downcase.split[1])
       parsed_api_response = JSON.parse(api_response)
-      # business_address = api_response['businesses'][0]['location']['address1']
-      # business_name = api_response['businesses'][0]['name']
-      # business_rating = api_response['businesses'][0]['rating']
       @response = ''
       @i = 0
       while @i < 4  do
         business_address = parsed_api_response['businesses'][@i]['location']['address1']
         business_name = parsed_api_response['businesses'][@i]['name']
         business_rating = parsed_api_response['businesses'][@i]['rating']
-        @response.concat("#{business_name} (#{business_address}, #{business_rating}). \n")
+        @response.concat("\n#{business_name} (#{business_address}, #{business_rating}). \n")
         @i +=1
       end
       return @response
