e2a2469d7f93de7e3653c6d0bb78a00796ae5e3a
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-03-01 11:09:34 -0800

    It will return info on first 4 businesses

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,9 +1,8 @@
 module ApiHelper
 
-  def get_restaurants(location, term)
+  def get_restaurants(term, location)
     begin
-      request = RestClient.get("https://api.yelp.com/v3/businesses/search?term=#{term}&location=#{location}", headers={"authorization": ENV['ACCESS_TOKEN'] })
-      request.truncate(30)
+      RestClient.get("https://api.yelp.com/v3/businesses/search?term=#{term}&location=#{location}", headers={"authorization": ENV['ACCESS_TOKEN'] })
     rescue
       return 'Yelp API ERROR'
     end
--- app/services/yelp_response.rb
+++ app/services/yelp_response.rb
@@ -7,7 +7,21 @@ class YelpResponse
 
   def process_response
     if @body.split.length == 2
-      get_restaurants(@body.downcase.split[0], @body.downcase.split[1])
+      api_response = get_restaurants(@body.downcase.split[0], @body.downcase.split[1])
+      parsed_api_response = JSON.parse(api_response)
+      # business_address = api_response['businesses'][0]['location']['address1']
+      # business_name = api_response['businesses'][0]['name']
+      # business_rating = api_response['businesses'][0]['rating']
+      @response = ''
+      @i = 0
+      while @i < 4  do
+        business_address = parsed_api_response['businesses'][@i]['location']['address1']
+        business_name = parsed_api_response['businesses'][@i]['name']
+        business_rating = parsed_api_response['businesses'][@i]['rating']
+        @response.concat("#{business_name}(#{business_address}, #{business_rating}). \n")
+        @i +=1
+      end
+      return @response
     else
       return "Please enter your request in the following format: 'Term' 'Location'. Ex: 'Delis Portland'"
     end
