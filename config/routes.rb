6213fa9d29afec3afc2165c80fbb4ba31fabd627
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-03-02 15:07:13 -0800

    Modify routes, nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ require 'sidekiq-scheduler'
 require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
 
-  root to: "home#index"
+  root to: "yelp#index"
+  get 'information', to: 'home#index', as: 'information'
   resource :messages, :only => [] do
     collection do
       post 'reply'
