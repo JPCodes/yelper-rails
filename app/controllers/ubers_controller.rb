6103edc47505498c08e5adb32c870421c05b5766
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-03-02 11:29:09 -0800

    Will show map based on yelp search selection

--- app/controllers/ubers_controller.rb
+++ app/controllers/ubers_controller.rb
@@ -4,9 +4,9 @@ class UbersController < ApplicationController
     @response = JSON.parse(getEstimatesForUserLocation)
     @fastest_ride = @response['prices'].sort_by{ |hash| hash['duration'] }.first['duration']/60
     # @get_map = get_google_map
-    @end_latitude = "40.7283405"
-    @end_longitude = "-73.994567"
-    @map = "http://maps.googleapis.com/maps/api/staticmap?zoom=17&format=png&sensor=false&size=280x280&maptype=roadmap&style=element:geometry.fill|color:0xf4f4f4&markers=color:red|" + @end_latitude + "," + @end_longitude + "&scale=2"
+    @end_latitude = params["latitude"]
+    @end_longitude = params["longitude"]
+    @map = "http://maps.googleapis.com/maps/api/staticmap?zoom=17&format=png&sensor=false&size=280x280&maptype=roadmap&style=element:geometry.fill|color:0xf4f4f4&markers=color:red|" + @end_latitude + "," + @end_longitude + "&scale=2&key=" + ENV['GOOGLE_API_KEY']
   end
 
   def get_route
--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -5,11 +5,15 @@ class YelpController < ApplicationController
 
   def create
     price_point = " " + search_params[:price_point]
-    @businesses = get_restaurants_price(search_params[:term], search_params[:city], price_point)
+    @businesses = JSON.parse(get_restaurants_price(search_params[:term], search_params[:city], price_point))
     respond_to do |format|
       format.html { redirect_to yelp_index_path }
       format.js
     end
+  end
+
+  def show
+
   end
 
   private
